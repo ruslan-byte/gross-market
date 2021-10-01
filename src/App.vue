25f8b8e0ddca02036f913f73a1a4f968a1572152
ruslan_byte <rbaroev0@gmail.com>
2021-10-01 15:50:49 +0300

    pull

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,17 @@
 		<Header />
 		<Instagram />
 		<Map />
+		<InformationPopup />
 	</div>
 </template>
 <script>
 	import Header from "@/components/Header"
 	import Instagram from "@/components/Instagram"
 	import Map from "@/components/Map"
+	import InformationPopup from "@/components/InformationPopup"
 	export default {
 		name: 'App',
-		components: {Header,Instagram, Map}
+		components: {Header,Instagram, Map,InformationPopup}
 	}
 </script>
 
@@ -22,6 +24,14 @@
 		font-family: 'EuclidCircularB';
 		src: url('./assets/fonts/EuclidCircularB-Medium.ttf');
 		font-weight: 500;
+		font-style: normal;
+		font-display: swap;
+	}
+	@font-face
+	{
+		font-family: 'EuclidCircularB';
+		src: url('./assets/fonts/EuclidCircularB-Regular.ttf');
+		font-weight: 400;
 		font-style: normal;
 		font-display: swap;
 	}
